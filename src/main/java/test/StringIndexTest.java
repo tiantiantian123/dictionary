0ea8300a1d30bbb9ca6bf909286053ef70c8ef25
thu <mingfei.net@gmail.com>
2016-05-10 14:05:46 +0800

    Update.

---
+++ src/main/java/test/StringIndexTest.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by Administrator
+ * 下午1:49 16-5-10
+ */
+public class StringIndexTest {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/util/DictSpider.java
+++ src/main/java/util/DictSpider.java
@@ -1,13 +1,15 @@
 package util;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import model.Word;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.ConnectException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,18 +24,58 @@ public class DictSpider {
     private static List<Word> words;
 
     private static void dump() {
+        Connection connection = DB.getConnection();
+        PreparedStatement preparedStatement = null;
+        String sql = "INSERT INTO db_dictionary.word VALUES(NULL, ?, ?, ?, ?)";
 
+        try {
+            if (connection == null) {
+                return;
+            }
+            connection.setAutoCommit(false);
+            preparedStatement = connection.prepareStatement(sql);
+
+            for (Word word : words) {
+                preparedStatement.setString(1, word.getEnglish());
+                preparedStatement.setString(2, word.getChinese());
+                preparedStatement.setString(3, word.getPhonetic());
+                preparedStatement.setString(4, word.getPartOfSpeech());
+
+                preparedStatement.addBatch();
+            }
+            preparedStatement.executeBatch();
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.close(null, preparedStatement, connection);
+        }
     }
 
     private static Word getWord(String english) {
         URL url = null;
         String chinese = null, phonetic = null, partOfSpeech = null;
+        InputStream inputStream = null;
         try {
             url = new URL(LINK.concat(english));
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+        if (url == null) {
+            return null;
+        }
+        try {
+            inputStream = url.openStream();
+        } catch (ConnectException e) {
+            getWord(english);
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (inputStream == null) {
+            return null;
+        }
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
 //                System.out.println(line);
@@ -42,14 +84,14 @@ public class DictSpider {
                     chinese = line.substring(line.indexOf("<strong>") + 8, line.indexOf("</strong>"));
                 }
                 if (line.contains("<span>英 <bdo>")) {
-                    phonetic = line.substring(line.indexOf("["), line.indexOf("]")+1);
+                    phonetic = line.substring(line.indexOf("["), line.indexOf("]") + 1);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         Word word = new Word(null, english, chinese, phonetic, partOfSpeech);
-        System.out.println(word);
+        System.out.println(english);
         return word;
     }
 
@@ -65,14 +107,17 @@ public class DictSpider {
     }
 
     public static void main(String[] args) {
-        englishList = new ArrayList<>();
-        getEnglishList();
-        words = new ArrayList<>();
-        for (String english : englishList) {
-            words.add(getWord(english));
-        }
-        dump();
+//        englishList = new ArrayList<>();
+//        getEnglishList();
+//        words = new ArrayList<>();
+//        for (String english : englishList) {
+//            Word word = getWord(english);
+//            if (word != null) {
+//                words.add(getWord(english));
+//            }
+//        }
+//        dump();
 
-//        getWord("absolute");
+        getWord("export");
     }
 }
